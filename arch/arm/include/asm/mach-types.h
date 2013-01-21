f9b11b13bdc850c2bba511dcf0a002d08bb1b138
Cyril Chemparathy <cyril@ti.com>
2013-01-21 11:19:30 -0500

    tmp: add support for keystone machine types
    This patch will be removed at a later point - pure device tree platforms
    should not need machine types defined.

--- arch/arm/include/asm/mach-types.h
+++ arch/arm/include/asm/mach-types.h
@@ -1,5 +1,5 @@
 /*
- * This was automagically generated from arch/arm/tools/mach-types!
+ * This was automagically generated from /home/a0875269/Projects/Kernel/linux/arch/arm/tools/mach-types!
  * Do NOT edit
  */
 
@@ -1107,6 +1107,10 @@ extern unsigned int __machine_arch_type;
 #define MACH_TYPE_OMAP5_SEVM           3777
 #define MACH_TYPE_ARMADILLO_800EVA     3863
 #define MACH_TYPE_KZM9G                4140
+#define MACH_TYPE_TCI6614_SIM          4000
+#define MACH_TYPE_TCI6614_EVM          4001
+#define MACH_TYPE_TCI6638_SIM          4010
+#define MACH_TYPE_TCI6638_EVM          4011
 
 #ifdef CONFIG_ARCH_EBSA110
 # ifdef machine_arch_type
@@ -14246,6 +14250,54 @@ extern unsigned int __machine_arch_type;
 # define machine_is_kzm9g()	(machine_arch_type == MACH_TYPE_KZM9G)
 #else
 # define machine_is_kzm9g()	(0)
+#endif
+
+#ifdef CONFIG_MACH_TCI6614_SIM
+# ifdef machine_arch_type
+#  undef machine_arch_type
+#  define machine_arch_type	__machine_arch_type
+# else
+#  define machine_arch_type	MACH_TYPE_TCI6614_SIM
+# endif
+# define machine_is_tci6614_sim()	(machine_arch_type == MACH_TYPE_TCI6614_SIM)
+#else
+# define machine_is_tci6614_sim()	(0)
+#endif
+
+#ifdef CONFIG_MACH_TCI6614_EVM
+# ifdef machine_arch_type
+#  undef machine_arch_type
+#  define machine_arch_type     __machine_arch_type
+# else
+#  define machine_arch_type	MACH_TYPE_TCI6614_EVM
+# endif
+# define machine_is_tci6614_evm()	(machine_arch_type == MACH_TYPE_TCI6614_EVM)
+#else
+# define machine_is_tci6614_evm()	(0)
+#endif
+
+#ifdef CONFIG_MACH_TCI6638_SIM
+# ifdef machine_arch_type
+#  undef machine_arch_type
+#  define machine_arch_type	__machine_arch_type
+# else
+#  define machine_arch_type	MACH_TYPE_TCI6638_SIM
+# endif
+# define machine_is_tci6638_sim()	(machine_arch_type == MACH_TYPE_TCI6638_SIM)
+#else
+# define machine_is_tci6638_sim()	(0)
+#endif
+
+#ifdef CONFIG_MACH_TCI6638_EVM
+# ifdef machine_arch_type
+#  undef machine_arch_type
+#  define machine_arch_type     __machine_arch_type
+# else
+#  define machine_arch_type	MACH_TYPE_TCI6638_EVM
+# endif
+# define machine_is_tci6638_evm()	(machine_arch_type == MACH_TYPE_TCI6638_EVM)
+#else
+# define machine_is_tci6638_evm()	(0)
 #endif
 
 /*
