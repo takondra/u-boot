c03d6a39efdd0f7957f8e225311c82ebc81d4f64
Vitaly Andrianov <vitalya@ti.com>
2013-06-27 08:37:44 -0400

    tci6638: [tmp] add setmpax shell command
    This patch adds a command to configure SES MPAX segments for ARM PrivId.
    It may be used to debug access to the entire DDR3A memory range.
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>

--- arch/arm/cpu/armv7/keystone/msmc.c
+++ arch/arm/cpu/armv7/keystone/msmc.c
@@ -74,3 +74,50 @@ void share_all_segments(int priv_id)
 	}
 }
 
+void map_ses_segment(int priv_id, int ses_pair,
+		     u32 src_pfn, u32 dst_pfn, mpax_seg_size size)
+{
+	struct msms_regs* msmc = (struct msms_regs*) TCI6638_MSMC_CTRL_BASE;
+
+	msmc->ses[priv_id][ses_pair].mpaxh = src_pfn << 12 | (size & 0x1f) | 0x80;
+	msmc->ses[priv_id][ses_pair].mpaxl = dst_pfn << 8 | 0x3f;
+}
+
+
+int do_setmpax_cmd(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	u32	src, dst, indx, size;
+
+
+	if (argc != 5)
+		goto mpax_cmd_usage;
+
+	indx = simple_strtoul(argv[1], NULL, 10);
+	src  = simple_strtoul(argv[2], NULL, 16);
+	dst  = simple_strtoul(argv[3], NULL, 16);
+	size = simple_strtoul(argv[4], NULL, 16);
+
+	if (indx > 7)
+		goto mpax_cmd_usage;
+
+	src &= 0xfffff;
+	dst &= 0xffffff;
+	size &= 0x1f;
+
+	if ((size < 0xb) && (size != 0))
+		goto mpax_cmd_usage;
+
+	map_ses_segment(8, indx, src, dst, size);
+
+	return 0;
+
+mpax_cmd_usage:
+	return cmd_usage(cmdtp);
+}
+
+U_BOOT_CMD(
+	setmpax,	5,	0,	do_setmpax_cmd,
+	"set mpax ses for ARM privid",
+	"<pair> <src_pfn in hex> <dst_pfn in hex> <size 0xb (4K), 0xc (8K) and so on up to 0x1F for 4G>\n"
+);
+
--- arch/arm/include/asm/arch-keystone/hardware.h
+++ arch/arm/include/asm/arch-keystone/hardware.h
@@ -136,6 +136,35 @@ static inline int cpu_revision(void)
 
 	return rev;
 }
+
+typedef enum {
+	MPAX_SEG_4K = 0x0b,
+	MPAX_SEG_8K,
+	MPAX_SEG_16K,
+	MPAX_SEG_32K,
+	MPAX_SEG_64K,
+	MPAX_SEG_128K,
+	MPAX_SEG_256K,
+	MPAX_SEG_512K,
+	MPAX_SEG_1M,
+	MPAX_SEG_2M,
+	MPAX_SEG_4M,
+	MPAX_SEG_8M,
+	MPAX_SEG_16M,
+	MPAX_SEG_32M,
+	MPAX_SEG_64M,
+	MPAX_SEG_128M,
+	MPAX_SEG_256M,
+	MPAX_SEG_512M,
+	MPAX_SEG_1G,
+	MPAX_SEG_2GM,
+	MPAX_SEG_4G
+} mpax_seg_size;
+
+void map_ses_segment(int priv_id, int ses_pair,
+		     u32 src_pfn, u32 dst_pfn, mpax_seg_size size);
+void share_all_segments(int priv_id);
+
 #endif
 
 #endif /* __ASM_ARCH_HARDWARE_H */
