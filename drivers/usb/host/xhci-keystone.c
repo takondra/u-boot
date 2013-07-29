4822d0d2d940e4d0a1edd0a7c0ab1a691b21cace
WingMan Kwok <w-kwok2@ti.com>
2013-07-29 18:58:43 -0400

    keystone: usb: update xhci host controller reset
    The previous reset procedure works only when VBUS is low, which is the
    case on VDB.  The updated procedure works regardless of the VBUS state.
    
    Signed-off-by: WingMan Kwok <w-kwok2@ti.com>

--- drivers/usb/host/xhci-keystone.c
+++ drivers/usb/host/xhci-keystone.c
@@ -47,12 +47,14 @@
 #define K_USB3_PHY_SSC_EN			BIT(31)
 #define K_USB3_PHY_REF_USE_PAD			BIT(30)
 #define K_USB3_PHY_REF_SSP_EN			BIT(29)
-#define K_USB3_PHY_FSEL			0x27
+#define K_USB3_PHY_FSEL				0x27
 #define K_USB3_PHY_FSEL_SHIFT			22
 #define K_USB3_PHY_RETENABLEEN			BIT(21)
 #define K_USB3_PHY_REFCLKSEL			0x2
 #define K_USB3_PHY_REFCLKSEL_SHIFT		19
 #define K_USB3_PHY_OTGDISABLE			BIT(15)
+#define K_USB3_PHY_OTG_VBUSVLDECTSEL		BIT(16)
+
 
 #define K_USB3_PHY_SSC_REF_CLK_SEL		0
 #define K_USB3_PHY_SSC_REF_CLK_SEL_SHIFT	4
@@ -108,18 +110,29 @@ struct kxhci_ctrl {
 
 struct kxhci_ctrl kxhci;
 
-static void keystone_xhci_phy_set(struct xhci_phy *xhci)
+static void keystone_xhci_phy_set(struct xhci_phy *phy)
 {
-	/* Configure the PHY */
-	writel(K_USB3_PHY_CLOCK_DEFAULT, &xhci->phy_clock);
+	u32 val;
 
-	writel(K_USB3_PHY_PLL_DEFAULT, &xhci->phy_pll);
+	/* VBUSVLDEXTSEL has a default value of 1 in BootCfg but shouldn't.
+	 * It should always be cleared because our USB PHY has an onchip VBUS
+	 * analog comparator.
+	 */
+	val = readl(&phy->phy_clock);
+	/* quit selecting the vbusvldextsel by default! */
+	val &= ~K_USB3_PHY_OTG_VBUSVLDECTSEL;
+	writel(val, &phy->phy_clock);
+
 }
 
 static void keystone_xhci_phy_unset(struct xhci_phy *phy)
 {
+	u32 val;
+
 	/* Disable the PHY REFCLK clock gate */
-	writel(0, phy->phy_clock);
+	val = readl(&phy->phy_clock);
+	val &= ~K_USB3_PHY_REF_SSP_EN;
+	writel(val, &phy->phy_clock);
 }
 
 void dwc3_set_mode(unsigned int *hcd, u32 mode)
@@ -223,13 +236,15 @@ int xhci_hcd_init(int index, struct xhci_hccr **ret_hccr,
 	struct xhci_phy *phy;
 	unsigned int *hcd;
 	int ret;
-	u32 rev;
+	u32 val;
 
 	usbss = (struct kdwc3_irq_regs *)KEYSTONE2_USB_SS_BASE;
 	phy = (struct xhci_phy *)KEYSTONE2_DEV_USB_PHY_BASE;
 
 	/* Enable the PHY REFCLK clock gate with phy_ref_ssp_en = 1 */
-	writel(K_USB3_PHY_REF_SSP_EN, phy->phy_clock);
+	val = readl(&(phy->phy_clock));
+	val |= K_USB3_PHY_REF_SSP_EN;
+	writel(val, &phy->phy_clock);
 
 	mdelay(100);
 
@@ -238,16 +253,16 @@ int xhci_hcd_init(int index, struct xhci_hccr **ret_hccr,
 
 	mdelay(100);
 
+	/* Initialize usb phy */
+	keystone_xhci_phy_set(phy);
+
 	/* soft reset usbss */
 	writel(1, &usbss->sysconfig);
 	while (readl(&usbss->sysconfig) & 1)
 		;
 
-	rev = readl(&usbss->revision);
-	debug("usbss revision %x\n", rev);
-
-	/* Initialize usb phy */
-	keystone_xhci_phy_set(phy);
+	val = readl(&usbss->revision);
+	debug("usbss revision %x\n", val);
 
 	/* Initialize usb core */
 	hcd = (unsigned int *)KEYSTONE2_USB_HOST_XHCI_BASE;
@@ -287,24 +302,49 @@ int keystone_xhci_phy_suspend(void)
 	uint32_t *portsc_1 = NULL;
 	uint32_t *portsc_2 = NULL;
 	unsigned int *hcd;
-	u32 val, usb2_pls, usb3_pls;
+	u32 val, usb2_pls, usb3_pls, event_q;
 	int loop_cnt = 0;
 
 	/* set register addresses */
 	hcor = kxhci.hcor;
 	hcd = kxhci.hcd;
 
-	val = DWC3_GCTL_PWRDNSCALE(0x4b0) | DWC3_GCTL_PRTCAPDIR(0x2) |
-		DWC3_GCTL_SCALEDOWN(0x3) | DWC3_GCTL_DISSCRAMBLE;
+	/* Bypass Scrambling and Set Shorter Training sequence for simulation */
+	val = DWC3_GCTL_PWRDNSCALE(0x4b0) | DWC3_GCTL_PRTCAPDIR(0x2);
 	dwc3_writel(hcd, DWC3_GCTL, val);
 
-	/* Put USB3.0 PHY in Suspend Mode */
-	val = DWC3_GUSB3PIPECTL_SUSPHY | (1 << 29);
+	/* GUSB2PHYCFG */
+	val = dwc3_readl(hcd, DWC3_GUSB2PHYCFG(0));
+	/* assert bit 6 (SusPhy) */
+	val |= DWC3_GUSB2PHYCFG_SUSPHY;
+	dwc3_writel(hcd, DWC3_GUSB2PHYCFG(0), val);
+
+	/* GUSB3PIPECTL */
+	val = dwc3_readl(hcd, DWC3_GUSB3PIPECTL(0));
+	/* assert bit 29 to allow PHY to go to suspend when idle
+	 * and cause the USB3 SS PHY to enter suspend mode
+	 */
+	val |= (BIT(29) | DWC3_GUSB3PIPECTL_SUSPHY);
 	dwc3_writel(hcd, DWC3_GUSB3PIPECTL(0), val);
 
-	/* Wait for mpll to become stable */
+	/* Steps necessary to allow controller to suspend even when
+	 * VBUS is HIGH:
+	 * - Init DCFG[2:0] (DevSpd) to: 1=FS
+	 * - Init GEVNTADR0 to point to an eventQ
+	 * - Init GEVNTSIZ0 to 0x0100 to specify the size of the eventQ
+	 * - Init DCTL::Run_nStop = 1
+	 */
+	dwc3_writel(hcd, DWC3_DCFG, 0x00020001);
+	/* TODO: local2global( (Uint32) eventQ )? */
+	dwc3_writel(hcd, DWC3_GEVNTADRLO(0), (u32)&event_q);
+	dwc3_writel(hcd, DWC3_GEVNTADRHI(0), 0);
+	dwc3_writel(hcd, DWC3_GEVNTSIZ(0), 0x4);
+	/* Run */
+	dwc3_writel(hcd, DWC3_DCTL, DWC3_DCTL_RUN_STOP);
+
 	mdelay(100);
 
+	/* Wait for USB2 & USB3 PORTSC::PortLinkState to indicate suspend */
 	portsc_1 = (uint32_t *) (&hcor->PortRegs[0].or_portsc);
 	portsc_2 = (uint32_t *) (&hcor->PortRegs[1].or_portsc);
 	usb2_pls = 0;
@@ -315,23 +355,31 @@ int keystone_xhci_phy_suspend(void)
 		usb3_pls = (readl(portsc_2) & PORT_PLS_MASK) >> 5;
 	} while (((usb2_pls != 0x4) || (usb3_pls != 0x4)) && loop_cnt < 1000);
 
-	if (usb2_pls != 0x4)
-		debug("USB2 Port Link State %d - NOT disabled\n", usb2_pls);
-	else if (usb3_pls != 4)
-		debug("USB3 Port Link State %d - NOT disabled\n", usb3_pls);
-	else
-		debug("USB2 and USB3 Port Link State - Disabled, loop_cnt=%d\n", loop_cnt);
+	if (usb2_pls != 0x4 || usb3_pls != 0x4) {
+		debug("USB suspend failed - PLS USB2=%02x, USB3=%02x\n",
+			usb2_pls, usb3_pls);
+		return -1;
+	}
+
+	debug("USB2 and USB3 PLS - Disabled, loop_cnt=%d\n", loop_cnt);
+	return 0;
 }
 
 void xhci_hcd_stop(int index)
 {
 	/* Disable USB */
-	keystone_xhci_phy_suspend();
+	if (keystone_xhci_phy_suspend())
+		return;
 
-	psc_disable_module(TCI6638_LPSC_USB);
+	if (psc_disable_module(TCI6638_LPSC_USB)) {
+		debug("PSC disable module USB failed!\n");
+		return;
+	}
 
 	/* Disable PHY */
 	keystone_xhci_phy_unset(kxhci.phy);
 
 	memset(&kxhci, 0, sizeof(struct kxhci_ctrl));
+
+	debug("xhci_hcd_stop OK.\n");
 }
