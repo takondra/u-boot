d274e3faf420dfeeb63cc71794921b51eaefee15
Murali Karicheri <m-karicheri2@ti.com>
2013-08-23 12:59:16 -0400

    tci6638: enable cache coherence with QM PDSP
    This patch enables cache coherence between ARM and QM PSPs. This is needed
    for the keystone hwqueue driver in kernel to operate in cache coherent
    mode
    
    Signed-off-by: Sandeep Nair <sandeep_n@ti.com>
    Signed-off-by: Murali Karicheri <m-karicheri2@ti.com>

--- arch/arm/cpu/armv7/keystone/init.c
+++ arch/arm/cpu/armv7/keystone/init.c
@@ -44,6 +44,7 @@ int arch_cpu_init(void)
 #ifdef CONFIG_SOC_TCI6638
 	share_all_segments(8);
 	share_all_segments(9);
+	share_all_segments(10); /* QM PDSP */
 	share_all_segments(11); /* PCIE */
 #endif	
 
